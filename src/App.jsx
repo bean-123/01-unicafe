759c6af54d3a7635f764f8d4f30aee539077257a
bean-123 <amy.platt@hotmail.com>
2025-08-26 12:42:38 +0300

    Step 1 from unicafe

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <>
+      <h1>Give feedback</h1>
+      <button>good</button>
+      <button>neutral</button>
+      <button>bad</button>
+
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all</p>
+      <p>average</p>
+      <p>positive</p>
+    </>
+  );
 };
 
 export default App;
