87e10bfed090ef04ac39c1f806556841dc2f4ae6
bean-123 <amy.platt@hotmail.com>
2025-08-26 14:41:47 +0300

    Add buttons and onClick to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,57 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  console.log(props);
+  return (
+    <>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="nautral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <Statistics text="all" value={total} />
+    </>
+  );
+};
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const handleGood = () => {
+    console.log("good", good);
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    setTotal(updatedGood + bad + neutral);
+  };
+
+  const handleNautral = () => {
+    console.log("neutral", neutral);
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    setTotal(updatedNeutral + bad + good);
+  };
+
+  const handleBad = () => {
+    console.log("bad", bad);
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    setTotal(updatedBad + good + neutral);
+  };
 
   return (
     <>
       <h1>Give feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNautral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
 
       <h1>Statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all</p>
+      <p>all {total}</p>
       <p>average</p>
       <p>positive</p>
     </>
